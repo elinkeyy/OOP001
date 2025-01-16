f189d81d1577033db5ec709b17cd6a2ba74a6e8f
Elīna Kičigina <elina.kicigina@ventspils.edu.lv>
2025-01-16 10:14:31 +0200

    eksamens

---
+++ eksamens.py
@@ -0,0 +1,8 @@
+class Doktorats:
+    def __init__(self, nosaukums, pacientuSkaits)
+        self.nosaukums = nosaukums
+        self.pacientuSkaits = pacientuSkaits
+    def ievade(self):
+        self.nosaukums=input("Ievadiet nosaukumu")
+        pacientuSkaits = input("Ievadiet pacientu skaitu")
+    
