6da72b4fac71e70da7594d3b89fdb347bf954b89
Elīna Kičigina <elina.kicigina@ventspils.edu.lv>
2025-01-22 15:04:27 +0200

    uzdevums

--- eksamens2uzdevums.py
+++ eksamens2uzdevums.py
@@ -15,9 +15,25 @@ class VidusskolasSkolotajs(Skolotajs):
     otraisPrieksmets="y prieksmets"
     pirmaisPrieksmetsKopSkaits=0
     otraisPrieksmetsKopSkaits=0
-    def CikStundasKopa(self):
+    def kopstundas(self):
         self.stunduSkaitsNedels=self.pirmaisPrieksmetsKopSkaits+self.otraisPrieksmetsKopSkaits
         return self.stunduSkaitsNedels
+    def izvade(self):
+        print(f"Vidusskolas (tips - {self.skolotajuTips}) māca {self.stunduSkaitsNedels}")
+def main():
+
+    uzvards = input("Ievadiet sākumskolas skolotāja uzvārdu: ")
+    klase = input("Ievadiet skolotāja klasi: ")
+    stundas = int(input("Ievadiet skolotāja stundu skaitu: "))
+    
+    sakumskolas_skolotajs = SakumskolasSkolotajs(uzvards, klase, stundas)
+    print(sakumskolas_skolotajs.izvade())
+
+    # Input for VidusskolasSkolotajs
+    uzvards = input("Ievadiet vidusskolas skolotāja uzvārdu: ")
+    stundas = int(input("Ievadiet pirmā priekšmeta stundu skaitu: "))
+    stundas = int(input("Ievadiet otrā priekšmeta stundu skaitu: "))
+
 ss1=SakumskolasSkolotajs()
 ss1.izvade()
 vs1=VidusskolasSkolotajs()
