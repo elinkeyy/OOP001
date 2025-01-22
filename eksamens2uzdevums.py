17318b0664a16818167f23edf5356e4fad4da42e
Elīna Kičigina <elina.kicigina@ventspils.edu.lv>
2025-01-22 15:56:44 +0200

    uzdevumi

--- eksamens2uzdevums.py
+++ eksamens2uzdevums.py
@@ -8,20 +8,22 @@ class SakumskolasSkolotajs(Skolotajs):
     def __init__(self):
         self.skolotajuTips=1
     def ievade(self):
-        self.uzvards = input()
-        self.klase = int(input("Ievadiet klasi"))
+        self.uzvards = input("Ievadiet uzvardu")
+        self.klase = (input("Ievadiet klasi"))
         self.stunduSkaitsNedels = int(input("Ievadiet stundu skaitu nedeļā!"))
     def izvade(self):
-        print(f"Sākumskolas (tips - {self.skolotajuTips}) māca {self.stunduSkaitsNedels}")
+        print(f"Sākumskolas (tips - {self.skolotajuTips}) skolotājs {self.uzvards} māca {self.stunduSkaitsNedels}")
 
 class VidusskolasSkolotajs(Skolotajs):
     pirmaisPrieksmets="x prieksmets"
     otraisPrieksmets="y prieksmets"
     pirmaisPrieksmetsKopSkaits=0
     otraisPrieksmetsKopSkaits=0
+    def __init__(self):
+        self.skolotajuTips=3
     def kopstundas(self):
         self.stunduSkaitsNedels = self.pirmaisPrieksmetsKopSkaits+self.otraisPrieksmetsKopSkaits
-        return
+        return self.stunduSkaitsNedels
     def ievade(self):
         self.uzvards = input("Ievadiet vidusskolas skolotāja uzvardu")
         self.pirmaisPrieksmets = input("Ievadiet pirmo priekšmetu!")
@@ -30,12 +32,11 @@ class VidusskolasSkolotajs(Skolotajs):
         self.otraisPrieksmetsKopSkaits = int(input("Uzrakstiet otrā priekšmeta stundu skaitu!"))
 
     def izvade(self):
-        print(f"Vidusskolas skolotājs {self.uzvards} (tips - {self.skolotajuTips}) māca šīs priekšmetus:")
-        print(f"{self.pirmaisPrieksmets} un {self.otraisPrieksmets} tiek pasniegti {self.kopstundas()} reizes nedeļā,")
+        print(f"Vidusskolas (tips - {self.skolotajuTips}) skolotājs {self.uzvards} māca šīs priekšmetus:{self.pirmaisPrieksmets} un {self.otraisPrieksmets}, kopā {self.kopstundas()}")
 
-ss1=SakumskolasSkolotajs()
-ss1.ievade()
-ss1.izvade()
+#ss1=SakumskolasSkolotajs()
+#ss1.ievade()
+#ss1.izvade()
 vs1=VidusskolasSkolotajs()
 vs1.ievade()
 vs1.izvade()
