10348442f003bdcb19185c767a98b0a5b6512c9b
Elīna Kičigina <elina.kicigina@ventspils.edu.lv>
2025-01-23 09:50:01 +0200

    uzdevums

--- uzdevums2023gada.py
+++ uzdevums2023gada.py
@@ -5,8 +5,19 @@ class kubs:
         self.krasasNosaukums = input("Ievadiet kuba krāsu")
     def izvade(self):
         self.aprekinatTilpumu = pow(self.malasGarums, 3)
-        print(f"Kuba krāsa - {self.krasasNosaukums}, tilpums - {self.aprekinatTilpumu}")
+        print(f"Kuba krāsa - {self.krasasNosaukums}, tilpums - {self.aprekinatTilpumu} cm kubā")
+    def kubg(self):
+        self.krasasNosaukums = "zaļa"
+        self.malasGarums = 10
+        print(f"kubg krāsa - {self.krasasNosaukums}, malas garums - {self.malasGarums} cm")
+    def kubr(self):
+        self.krasasNosaukums = "sarkana"
+        self.malasGarums = 1
+        print (f"kubr krāsa - {self.krasasNosaukums}, malas garums - {self.malasGarums} cm")
+    del kubr
 
 k1 = kubs()
 k1.ievade()
 k1.izvade()
+k1.kubg()
+k1.kubr()
