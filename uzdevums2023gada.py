6f01dd5e015d18d205c6c23ed38849a890de79ec
Elīna Kičigina <elina.kicigina@ventspils.edu.lv>
2025-01-23 09:02:48 +0200

    uzdevums

--- uzdevums2023gada.py
+++ uzdevums2023gada.py
@@ -1,1 +1,12 @@
+import math
+class kubs:
+    def ievade(self):
+        self.malasGarums = int(input("Ievadiet malas garumu no 2 līdz 10"))
+        self.krasasNosaukums = input("Ievadiet kuba krāsu")
+    def izvade(self):
+        self.aprekinatTilpumu = pow(self.malasGarums, 3)
+        print(f"Kuba krāsa - {self.krasasNosaukums}, tilpums - {self.aprekinatTilpumu}")
 
+k1 = kubs()
+k1.ievade()
+k1.izvade()
